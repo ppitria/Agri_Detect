9a7182cc35b4abca812f83aa6af45afc9644f811
Pitriani <ppitri05@gmail.com>
2024-06-28 20:44:12 +0700

    Update

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/intro')
-def intro():
-    return render_template('intro.html')
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
